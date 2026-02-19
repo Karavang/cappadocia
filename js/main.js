124dce29392fa55adf8f60f4c8deb3543f072c14
Karavang <shestakevgeny10@gmail.com>
2026-02-19 17:07:57 +0100

    p2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(function () {
-  $(".fairy-tail_slider").slick({
+  $(".fairy-tail_slider, .our-trip_slider").slick({
     prevArrow:
       '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-      autoplay:true,
-      autoplaySpeed:3000
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+  });
+ Fancybox.bind("[data-fancybox]", {  
   });
 });
