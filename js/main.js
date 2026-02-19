df69a63961a62f251c4a17ae8f4e95350e5c9d23
Karavang <shestakevgeny10@gmail.com>
2026-02-19 13:53:58 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".fairy-tail_slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+      autoplay:true,
+      autoplaySpeed:3000
+  });
+});
